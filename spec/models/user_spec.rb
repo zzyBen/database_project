4f45e971722beb4478b35913d57aec5a3fd29a67
zzyBen <897224436@qq.com>
2017-12-22 00:33:56 +0800

    add extra exercise of test of lower-case saving of email and anoter saving way of lower-case email and reg of none .. email format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,6 +44,16 @@ RSpec.describe User, type: :model do
     end
   end
   
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    
+    it "should be saved as all-lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
